1ccc2bb0106a12d9a951b7b325e22ca496eea66c
Denis Kipyegon <57701113+kipyek@users.noreply.github.com>
2025-08-19 13:10:16 +0300

    search,filter,sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import CoinCard from './components/CoinCard'
+import FilterInput from './components/FilterInput';
+import LimitSelector from './components/LimitSelector';
+import SortSelector from './components/SortSelector';
 
 
 const API_URL = import.meta.env.VITE_COINS_API_URL
 
 const App = () => {
-  const [coins, setCoins] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState('');
+  const [sortBy, setSortBy] = useState('market_cap_desc');
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`)
         const data = await res.json();
         setCoins(data)
         console.log("coins", data)
@@ -23,18 +29,55 @@ const App = () => {
       }
     }
     fetchCoins();
-  }, [])
+  }, [limit])
+
+const filteredCoins = coins
+.filter((coin)=>{
+  return(
+    coin.name.toLowerCase().includes(filter.toLowerCase()) ||
+    coin.symbol.toLowerCase().includes(filter.toLowerCase())
+  )
+  })
+  .slice()
+  .sort((a,b)=>{
+          switch (sortBy) {
+        case 'market_cap_desc':
+          return b.market_cap - a.market_cap;
+        case 'market_cap_asc':
+          return a.market_cap - b.market_cap;
+        case 'price_desc':
+          return b.current_price - a.current_price;
+        case 'price_asc':
+          return a.current_price - b.current_price;
+        case 'change_desc':
+          return b.price_change_percentage_24h - a.price_change_percentage_24h;
+        case 'change_asc':
+          return a.price_change_percentage_24h - b.price_change_percentage_24h;
+      }
+  })
+
 
   return (
     <div>
       <h1>🚀 Crypto Dash</h1>
       {loading && <p>Loading...</p>}
       {error && <div className='error'>{error}</div>}
+
+      <div className='top-controls'>
+        <FilterInput filter={filter} onFilterChange={setFilter} />
+        <LimitSelector limit={limit} onLimitChange={setLimit} />
+        <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
+      </div>
+
       {!loading && !error && (
         <main className='grid'>
           {
-            coins.map((coin) => (
-              <CoinCard key={coin.id} coin={coin} />))}
+            filteredCoins.length > 0 ? (
+            filteredCoins.map((coin) => <CoinCard key={coin.id} coin={coin} />)
+          ) : (
+            <p>No matching coins</p>
+          )
+        }
         </main>)}
 
     </div>
