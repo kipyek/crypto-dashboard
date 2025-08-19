6eb91d107ea3b23cc28eaae0565f8d153ce4a61e
Denis Kipyegon <57701113+kipyek@users.noreply.github.com>
2025-08-19 12:48:50 +0300

    coincards with data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import CoinCard from './components/CoinCard'
+
+
+const API_URL = import.meta.env.VITE_COINS_API_URL
 
 const App = () => {
+  const [coins, setCoins] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+        const data = await res.json();
+        setCoins(data)
+        console.log("coins", data)
+      } catch (err) {
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchCoins();
+  }, [])
+
   return (
-    <div>App</div>
+    <div>
+      <h1>🚀 Crypto Dash</h1>
+      {loading && <p>Loading...</p>}
+      {error && <div className='error'>{error}</div>}
+      {!loading && !error && (
+        <main className='grid'>
+          {
+            coins.map((coin) => (
+              <CoinCard key={coin.id} coin={coin} />))}
+        </main>)}
+
+    </div>
   )
 }
 
